bf403b5ba8c9b0f800a5a8f02c4e045ae87a0e0d
Ana Martins <ana.af.martins@gmail.com>
2021-10-05 16:26:00 +0200

    user loc page done! on to Osvaldo for maps

--- client/src/pages/OrganizationsNearUserPage.jsx
+++ client/src/pages/OrganizationsNearUserPage.jsx
@@ -10,11 +10,12 @@ export default function OrganizationsNearUserPage(props) {
   const [user, setUser] = useState({});
   const [orgs, setOrgs] = useState([]);
   const [orgsForMap, setOrgsForMap] = useState([]);
-  let neededOrgs = [];
+
   // Get the token from the localStorage
   const storedToken = localStorage.getItem("authToken");
 
-  const getUserAndOrgs = (userId) => {
+  const getUser = (userId) => {
+    // Send the token through the request "Authorization" Headers
     axios
       .get(`${API_URL}/users/${userId}`, {
         headers: {
@@ -23,93 +24,52 @@ export default function OrganizationsNearUserPage(props) {
       })
       .then((response) => {
         setUser(response.data);
-        console.log("user data", response.data);
-        axios
-          .get(`${API_URL}/orgs`, {
-            headers: {
-              Authorization: `Bearer ${storedToken}`,
-            },
-          })
-          .then((response) => {
-            // setOrgs(response.data);
-            console.log(response.data);
-            // const orgs = response.data;
-
-            response.data.map((org) => {
-              if (org.country === user.country) {
-                console.log(org.country, user.country);
-                neededOrgs.push(org);
-              }
-              // return neededOrgs;
-            });
-
-            console.log("orgs after mapping", neededOrgs);
-            
-          })
-          .catch((error) => console.log(error));
-      });
+      })
+      .catch((error) => console.log(error));
   };
-  // const getUser = (userId) => {
-  //   // Send the token through the request "Authorization" Headers
-  //   axios
-  //     .get(`${API_URL}/users/${userId}`, {
-  //       headers: {
-  //         Authorization: `Bearer ${storedToken}`,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       setUser(response.data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // };
 
-  // const getAllOrgs = () => {
-  //   axios
-  //     .get(`${API_URL}/orgs`, {
-  //       headers: {
-  //         Authorization: `Bearer ${storedToken}`,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       setOrgs(response.data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // };
+  const getAllOrgs = () => {
+    axios
+      .get(`${API_URL}/orgs`, {
+        headers: {
+          Authorization: `Bearer ${storedToken}`,
+        },
+      })
+      .then((response) => {
+        setOrgs(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-  // function mapOrgsArray() {
-  //   orgs.map((org) => {
-  //     console.log(org.country, user.country);
-  //     if (org.country === user.country) {
-  //       neededOrgs.push(org);
-  //     }
-  //   });
+  function mapOrgsArray() {
+    let neededOrgs = [];
+    orgs.map((org) => {
+      console.log(org.country, user.country);
+      if (org.country === user.country) {
+        neededOrgs.push(org);
+      }
+    });
 
-  //   setOrgsForMap(neededOrgs);
-  //   console.log(neededOrgs);
-  // }
-
-  // useEffect(() => {
-  //   mapOrgsArray();
-  // }, []);
-
-  // useEffect(() => {
-  //   getAllOrgs();
-  //   console.log(neededOrgs);
-  // }, []);
-
-  // useEffect(() => {
-  //   getUser(userId);
-  // }, [userId]);
+    setOrgsForMap(neededOrgs);
+  }
 
   useEffect(() => {
-    getUserAndOrgs(userId);
-  }, [props]);
+    getAllOrgs();
+  }, []);
+
+  useEffect(() => {
+    getUser(userId);
+  }, [userId]);
+
+  useEffect(() => {
+    mapOrgsArray();
+  }, [orgs]);
 
   return (
     <div>
       <h1>Organizations near you</h1>
       {orgsForMap.map((org) => {
-        <p>{org.country}</p>;
+        return <p>{org.country}</p>;
       })}
     </div>
   );

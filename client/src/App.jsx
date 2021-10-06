2b556502e446a83122a90753888c26c957670f44
Ana Martins <ana.af.martins@gmail.com>
2021-10-06 15:14:19 +0200

    started styling of about page, continuation of orgs details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import "./components/stars.css";
+
 import { Route, Switch } from "react-router-dom";
 import { useState, useContext } from "react";
 import axios from "axios";
-import "./components/stars.css";
+
 import PrivateRoute from "./components/PrivateRoute";
 import AnonRoute from "./components/AnonRoute";
 import Navbar from "./components/Navbar";
@@ -26,51 +28,47 @@ import Footer from "./components/Footer";
 
 import { AuthContext } from "./contexts/auth.context";
 
-
-
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
 
 function App(props) {
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const { logInUser } = useContext(AuthContext);
- 
+
   const handleGoogleSuccess = (data) => {
-    const {givenName, email} = data.profileObj
-    
+    const { givenName, email } = data.profileObj;
+
     const requestBody = {
       email,
       name: givenName,
-      password: "GoogleUser1234"
-    }
+      password: "GoogleUser1234",
+    };
 
     axios
-      .post(`${API_URL}/auth/google`, requestBody )
-        .then((response) =>{ 
-          const token = response.data.authToken;
-          logInUser(token);
-          props.history.push("/")
-        })
-        .catch((error) => {
-          const errorDescription = error;
-          setErrorMessage(errorDescription);
-        });
-  } 
+      .post(`${API_URL}/auth/google`, requestBody)
+      .then((response) => {
+        const token = response.data.authToken;
+        logInUser(token);
+        props.history.push("/");
+      })
+      .catch((error) => {
+        const errorDescription = error;
+        setErrorMessage(errorDescription);
+      });
+  };
 
   const handleGoogleFailure = (err) => {
-    console.log("Error with google singup", err) 
-    setErrorMessage(err)
-  }
-
-
+    console.log("Error with google singup", err);
+    setErrorMessage(err);
+  };
 
   return (
     <div className="App">
-        <Navbar
-          onGoogleSuccess={handleGoogleSuccess}
-          onGoogleFailure={handleGoogleFailure}
-         />
-        <div className="mainContainer">    
+      <Navbar
+        onGoogleSuccess={handleGoogleSuccess}
+        onGoogleFailure={handleGoogleFailure}
+      />
+      <div className="mainContainer">
         <Switch>
           <PrivateRoute
             exact
--- client/src/pages/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import "../styles/aboutUsPage.css";
 
 export default function AboutUs() {
   return (
-    <div>
-      <img src="/images/about-us-header.png" alt="Rocket Lady" />
+    <div className="aboutUsPage">
+      <img
+        src="/images/about-us-header.png"
+        alt="Rocket Lady"
+        className="aboutUsImg"
+      />
+      <p>
+        A repository of women's rights organizations, Igniters is a power tool
+        for women around the world! 💪
+        <br />
+        Everyone can search for organizations and users are able to save useful
+        resources, review organizations, as well as create a page for their own
+        organization.
+      </p>
     </div>
   );
 }

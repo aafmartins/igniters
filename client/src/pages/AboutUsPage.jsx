cdbaea6aa39b720b2ce5339a2eb775a1066b907b
Monika Geiger <geigermonika@hotmail.hu>
2021-10-07 10:37:46 +0200

    heroku changes

--- client/src/pages/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage.jsx
@@ -8,14 +8,22 @@ export default function AboutUs() {
         alt="Rocket Lady"
         className="images"
       />
-      <p>
+      <p className="aboutUsDescription">
         A repository of women's rights organizations, Igniters is a power tool
         for women around the world! 💪
         <br />
         Everyone can search for organizations and users are able to save useful
         resources, review organizations, as well as create a page for their own
         organization.
+        <br />
+<section className="usedPages">
+          <a href="https://icons8.com/illustrations/author/5c07e68d82bcbc0092519bb6">
+            Icons 8
+        </a>{" "}
+          from <a href="https://icons8.com/illustrations">Ouch!</a>
+</section>
       </p>
+
     </div>
   );
 }
